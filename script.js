95e41d03b465e566b3eb3684d296c794ae60e7e9
LuizOliver123 <ogusta321@gmail.com>
2022-07-13 19:40:33 -0300

    thatsok

---
+++ script.js
@@ -0,0 +1,80 @@
+var one = document.getElementById('one');
+var two = document.getElementById('two');
+var three = document.getElementById('three');
+var four = document.getElementById('four');
+var oneo = document.getElementById('oneo');
+var twot = document.getElementById('twot');
+var threet = document.getElementById('threet');
+var fourf = document.getElementById('fourf');
+
+one.addEventListener("mouseover", function(){
+    oneo.style.color = "#ff2d75";
+    oneo.style.transition = "0.4s";
+    one.style.transform = "scale(1.1)";
+    one.style.transition = "0.4s";
+});
+
+one.addEventListener("mouseout", function(){
+    oneo.style.color = "white";
+    oneo.style.transition = "0.4s";
+    one.style.transform = "scale(1.0)";
+    one.style.transition = "0.3s";
+});
+
+
+
+two.addEventListener("mouseover", function(){
+    twot.style.color = "#ff2d75";
+    twot.style.transition = "0.4s";
+    two.style.transform = "scale(1.1)";
+    two.style.transition = "0.4s";
+});
+
+two.addEventListener("mouseout", function(){
+    twot.style.color = "white";
+    twot.style.transition = "0.4s";
+    two.style.transform = "scale(1.0)";
+    two.style.transition = "0.3s";
+});
+
+
+
+
+
+
+
+three.addEventListener("mouseover", function(){
+    threet.style.color = "#ff2d75";
+    threet.style.transition = "0.4s";
+    three.style.transform = "scale(1.1)";
+    three.style.transition = "0.4s";
+});
+
+three.addEventListener("mouseout", function(){
+    threet.style.color = "white";
+    threet.style.transition = "0.4s";
+    three.style.transform = "scale(1.0)";
+    three.style.transition = "0.3s";
+});
+
+
+
+
+
+
+
+
+
+four.addEventListener("mouseover", function(){
+    fourf.style.color = "#ff2d75";
+    fourf.style.transition = "0.4s";
+    four.style.transform = "scale(1.1)";
+    four.style.transition = "0.4s";
+});
+
+four.addEventListener("mouseout", function(){
+    fourf.style.color = "white";
+    fourf.style.transition = "0.4s";
+    four.style.transform = "scale(1.0)";
+    four.style.transition = "0.3s";
+});
